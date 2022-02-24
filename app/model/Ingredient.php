f68956a81368f696b36d89016610910df9c287d0
Marco B <m.brockmann@reanmo.com>
2022-02-24 13:05:07 +0100

    Refactoring: Intendation

--- app/model/Ingredient.php
+++ app/model/Ingredient.php
@@ -18,12 +18,11 @@
         }
 
         public function getIngredientsByProductId($prod_id) {
-            $this->database->query('
-                SELECT ingredients.ID, ingredients.name
-                FROM products
-                JOIN products_ingredients ON products.ID = products_ingredients.product_ID
-                JOIN ingredients ON products_ingredients.ingredient_ID = ingredients.ID
-                WHERE products.ID = :prod_id;
+            $this->database->query('SELECT ingredients.ID, ingredients.name
+                                    FROM products
+                                    JOIN products_ingredients ON products.ID = products_ingredients.product_ID
+                                    JOIN ingredients ON products_ingredients.ingredient_ID = ingredients.ID
+                                    WHERE products.ID = :prod_id;
             ');
             $this->database->bind(':prod_id', $prod_id);
             return $this->database->resultSet();
