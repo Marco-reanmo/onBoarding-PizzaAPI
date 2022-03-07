e6f5dbb1094e339978f0dee4ee636a3eba9d802f
Marco B <m.brockmann@reanmo.com>
2022-03-07 14:33:49 +0100

    Bugfix: Images from DB are now displayed

--- app/view/products/details.php
+++ app/view/products/details.php
@@ -1,6 +1,6 @@
 <?php require_once APPROOT . 'view/inc/header.php';?>
 <h1><?php echo $data['product']->name?></h1>
-<img src=<?php echo !$data['product_image'] == NULL ? $data['product_image']->image : URLROOT . 'public/images/default_pizza.png'?> alt="<?php echo $data['product']->name?>">
+<img src="<?php echo !$data['product_image'] == NULL ? 'data:image/jpeg;base64,' . base64_encode($data['product_image']->image) : URLROOT . 'public/images/default_pizza.png'?>" alt="<?php echo $data['product']->name?>">
 <h3>Zutaten</h3>
 <ul>
     <?php foreach($data['ingredients'] as $ingredient):?>
